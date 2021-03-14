9117533e9b1647941e60a543787a5953227b478c
Mateusz Piotr Moruś <mpmorus@gmail.com>
2021-03-14 11:43:44 +0100

    Add postgres container to docker-compose

--- backend/lights/settings.py
+++ backend/lights/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'lights.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': 'database',
+        'PORT': 5432,
     }
 }
 
