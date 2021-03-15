a987961c6fe5bfa0cf6b5047b3f870b47d7afcac
dorotajulia <62723006+dorotajulia@users.noreply.github.com>
2021-03-15 21:24:32 +0100

    Add Code model

--- backend/codes/admin.py
+++ backend/codes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Code, CustomUser
 
-# Register your models here.
+
+admin.site.register(Code)
+admin.site.register(CustomUser)
--- backend/codes/models.py
+++ backend/codes/models.py
@@ -4,3 +4,21 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Languages(models.IntegerChoices):
+    JAVASCRIPT = 0
+    PYTHON = 1
+
+
+class Code(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    code = models.TextField(max_length=10000)
+    approved = models.BooleanField(default=False)
+    date_approved = models.DateTimeField(null=True)
+    language = models.IntegerField(choices=Languages.choices, default=Languages.JAVASCRIPT)
+    is_example = models.BooleanField(default=False)
