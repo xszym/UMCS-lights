cec9802a95c5421ccb451297178de03bd37955f3
Mateusz Piotr Moruś <mpmorus@gmail.com>
2021-03-20 20:02:17 +0100

    Add parametrized "code create" tests

--- backend/tests/codes/test_views.py
+++ backend/tests/codes/test_views.py
@@ -7,20 +7,39 @@ from codes.models import Code
 
 
 @pytest.mark.django_db
-def test_add_code(client):
+@pytest.mark.parametrize('data,expected_status,expected_object_count',
+    [
+        ({'name': 'TestCorrect', 'author': 'Maciej', 'code': 'print(1)'}, status.HTTP_201_CREATED, 1),
+        ({'name': 'SomeWeirdField', 'SomeWeirdField': 'SomeWeirdValue', 'author': 'Maciej', 'code': 'print(1)'}, status.HTTP_201_CREATED, 1),
+        ({'name': 'MultilineCodeLF', 'author': 'Mateusz', 'code': '\n\nprint(1)\nprint(2)\n'}, status.HTTP_201_CREATED, 1),
+        ({'name': 'MultilineCodeCRLF', 'author': 'Mateusz', 'code': '\r\n\r\nprint(1)\r\nprint(2)\r\n'}, status.HTTP_201_CREATED, 1),
+        ({'name': '', 'author': 'Mateusz', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'author': 'Mateusz', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'TestEmptyCode', 'author': 'Mateusz', 'code': ''}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'TestEmptyCode', 'author': 'Mateusz'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'TestEmptyAuthor', 'author': '', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'TestEmptyAuthor', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': '', 'author': '', 'code': ''}, status.HTTP_400_BAD_REQUEST, 0),
+        ({}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': '\0', 'author': 'Maciej', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'NameWithNull\0', 'author': 'Maciej', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'NameWith\0Null', 'author': 'Maciej', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'AuthorWithNull', 'author': '\0', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'AuthorWithNull', 'author': 'Mateusz\0', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'AuthorWithNull', 'author': 'Mate\0sz', 'code': 'print(1)'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'CodeWithNull', 'author': 'Mateusz', 'code': '\0'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'CodeWithNull', 'author': 'Mateusz', 'code': 'print(1)\0'}, status.HTTP_400_BAD_REQUEST, 0),
+        ({'name': 'CodeWithNull', 'author': 'Mateusz', 'code': 'print(\0)'}, status.HTTP_400_BAD_REQUEST, 0),
+    ]
+)
+def test_add_code(client, data, expected_status, expected_object_count):
     url = reverse('code-list')
-    response = client.post(
-        url,
-        {
-            'name': 'Test',
-            'author': 'maciej',
-            'code': 'print(1)',
-        }
-    )
+    response = client.post(url, data)
 
-    assert response.status_code == status.HTTP_201_CREATED
-    assert response.data['name'] == 'Test'
-    assert Code.objects.count() == 1
+    assert response.status_code == expected_status
+    if response.status_code == status.HTTP_201_CREATED:
+        assert response.data['name'] == data['name']
+    assert Code.objects.count() == expected_object_count
 
 
 @pytest.mark.django_db
