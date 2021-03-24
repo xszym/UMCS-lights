48e82ae5275f3e1f558654c06e40b7cfb9771808
xszym <xszym69@gmail.com>
2021-03-24 13:45:35 +0100

    Test CD

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 (
   cd "$DIR/.."
 
-  ssh -i $ID_RSA $SERVER_USER@$SERVER_IP -p $SERVER_SSH_PORT -o StrictHostKeyChecking=no <<-EOF
+  ssh -i $ID_RSA -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_IP -p $SERVER_SSH_PORT <<-EOF
     cd ~/UMCS-lights
     git pull
     docker login -u gitlab-ci-token -p $CI_BUILD_TOKEN $CI_REGISTRY
