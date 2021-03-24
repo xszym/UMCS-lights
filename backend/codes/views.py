fd644fcdf6aed70e51ea4cb1ffbfca185c065f59
Mateusz Piotr Moruś <mpmorus@gmail.com>
2021-03-24 17:23:49 +0100

    Add api routes to retrieve codes

--- backend/codes/views.py
+++ backend/codes/views.py
@@ -3,11 +3,47 @@ from .serializers import CodeSerializer
 from .models import Code
 
 
-class CodeViewSet(viewsets.ModelViewSet):
+class AllCodesViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post']
+    serializer_class = CodeSerializer
+
+    def get_queryset(self):
+        queryset = Code.objects
+        return queryset
+
+class ExampleCodesViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     http_method_names = ['get', 'post']
     serializer_class = CodeSerializer
 
     def get_queryset(self):
         queryset = Code.objects.filter(is_example=True)
+        return queryset
+
+class NonExampleCodesViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post']
+    serializer_class = CodeSerializer
+
+    def get_queryset(self):
+        queryset = Code.objects.filter(is_example=False)
+        return queryset
+
+class ApprovedCodesViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post']
+    serializer_class = CodeSerializer
+
+    def get_queryset(self):
+        queryset = Code.objects.filter(approved=True)
+        return queryset
+
+class UnapprovedCodesViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post']
+    serializer_class = CodeSerializer
+
+    def get_queryset(self):
+        queryset = Code.objects.filter(approved=False)
         return queryset
--- backend/lights/urls.py
+++ backend/lights/urls.py
@@ -7,7 +7,11 @@ from .views import ping
 
 
 router = routers.DefaultRouter()
-router.register(r'code/?', codes_views.CodeViewSet, basename='code')
+router.register(r'codes/all', codes_views.AllCodesViewSet, basename='codes/all')
+router.register(r'codes/approved', codes_views.ApprovedCodesViewSet, basename='codes/approved')
+router.register(r'codes/unapproved', codes_views.UnapprovedCodesViewSet, basename='codes/unapproved')
+router.register(r'codes/example', codes_views.ExampleCodesViewSet, basename='codes/example')
+router.register(r'codes/nonexample', codes_views.NonExampleCodesViewSet, basename='codes/nonexample')
 
 
 urlpatterns = [
