01f474917c7e7cff408d17a4ea5deaf7b7b954dd
Mateusz Piotr Moruś <mpmorus@gmail.com>
2021-04-27 16:31:07 +0200

    Get redis host from env and standarize quotemarks

--- backend/lights/views.py
+++ backend/lights/views.py
@@ -1,17 +1,20 @@
 from django.http import JsonResponse
 import redis
+import os
 
 
-redis_db = redis.Redis(host='redis', port=6379)
+redis_db = redis.Redis(
+    host=os.environ['REDIS_HOST'],
+    port=int(os.environ['REDIS_PORT'])
+)
 
 
 def getDMXvalues(request):
-    dmx_values = redis_db.get("DMXvalues").decode('utf-8')
-    data = {"DMXvalues": dmx_values}
+    dmx_values = redis_db.get('DMXvalues').decode('utf-8')
+    data = {'DMXvalues': dmx_values}
     return JsonResponse(data)
 
 
 def ping(request):
-    data = {"ping": "pong!"}
+    data = {'ping': 'pong!'}
     return JsonResponse(data)
-    
