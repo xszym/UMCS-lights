97fcc56b482ec7b7c68beeffff587dac9afb3116
Maciej Prostak <maciej.prostak@embiq.com>
2021-05-06 17:09:11 +0200

    Testing

--- emulate/sender.py
+++ emulate/sender.py
@@ -12,7 +12,7 @@ async def send_dmx_values(websocket, path):
 	while True:
 		dmx_values = redis_db.get('DMXvalues').decode('utf-8')
 		await websocket.send(dmx_values)
-		await asyncio.sleep(1/20)
+		await asyncio.sleep(46.0/1000.0)
 
 start_server = websockets.serve(
 	send_dmx_values,
