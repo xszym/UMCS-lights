6f043c31fd9835a7daa4c2661a653ec7e96bd9a1
Maciej Prostak <maciej.prostak@embiq.com>
2021-05-11 16:47:47 +0200

    Add debugging to dmx_client

--- dmx_client/dmx_client.py
+++ dmx_client/dmx_client.py
@@ -11,8 +11,24 @@ import numpy
 load_dotenv()
 
 
+class PyDMXMock:
+	def __init__(self):
+		self.d = [0] * 28 * 5 * 3
+
+	def set_data(self, i, value):
+		self.d[i] = value
+
+	def send(self):
+		print(f'{self.d}\n')
+
+
 DEVICE_NAME = os.environ.get('DEVICE_NAME', '/dev/ttyUSB0')
-dmx_device = PyDMX.PyDMX(DEVICE_NAME)
+DEBUG = os.environ.get('DEBUG') == 'True'
+
+if DEBUG is False:
+	dmx_device = PyDMX.PyDMX(DEVICE_NAME)
+else:
+	dmx_device = PyDMXMock()
 
 SERVER_IP = os.environ.get('SERVER_IP', 'localhost')
 SERVER_PORT = os.environ.get('WEBSOCKETS_SERVER_PORT', 5678)
@@ -29,7 +45,7 @@ async def handler():
 			dmx_values = dmx_values[::-1]
 
 			dmx_values = [max(0, min(int(x), 255)) for x in dmx_values]
-			
+
 			for i in range(0, len(dmx_values), 3):
 				dmx_device.set_data(i+1, dmx_values[i+2])
 				dmx_device.set_data(i+2, dmx_values[i+1])
