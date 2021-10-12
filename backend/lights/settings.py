17ddb8af962b686d2f70a173e43567c96d4557ad
Szymon Szostak <21984800+xszym@users.noreply.github.com>
2021-10-13 00:30:06 +0200

    Refactor and clean code

--- backend/lights/settings.py
+++ backend/lights/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'lights.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        'ENGINE': "django.db.backends.postgresql",
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
